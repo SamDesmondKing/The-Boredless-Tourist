0e9174f640b8b4c64346b106d99ee061a076dbb0
Sam King <samuel-king@live.com.au>
2019-01-06 17:43:16 +1100

    Added interest finder logic

--- The-Boredless-Tourist.py
+++ The-Boredless-Tourist.py
@@ -1,8 +1,9 @@
-# -- The Boredless Tourist --
+# -- The Boredless Tourist --------------------------
 #
 # Tourism recommendation engine built for CodeCademy.
 # My first Python program.
 #
+# ---------------------------------------------------
 
 destinations = ['Paris, France', 'Shanghai, China', 'Los Angeles, USA', 'Sao Paulo, Brazil', 'Cairo, Egypt']
 
@@ -22,21 +23,18 @@ def get_traveler_location(traveler):
   return traveler_destination_index
 
 def add_attraction(destination, attraction):
-
   try:
     destination_index = get_destination_index(destination)
     attractions_for_destination = attractions[destination_index]
     attractions_for_destination.append(attraction)
     return
-
   except ValueError:
     return
-
 
 add_attraction('Los Angeles, USA', ['Venice Beach', ['beach']])
 add_attraction("Paris, France", ["the Louvre", ["art", "museum"]])
 add_attraction("Paris, France", ["Arc de Triomphe", ["historical site", "monument"]])
-add_attraction("Shanghai, China", ["Yu Garden", ["garden", "historcical site"]])
+add_attraction("Shanghai, China", ["Yu Garden", ["garden", "historical site"]])
 add_attraction("Shanghai, China", ["Yuz Museum", ["art", "museum"]])
 add_attraction("Shanghai, China", ["Oriental Pearl Tower", ["skyscraper", "viewing deck"]])
 add_attraction("Los Angeles, USA", ["LACMA", ["art", "museum"]])
@@ -44,4 +42,26 @@ add_attraction("São Paulo, Brazil", ["São Paulo Zoo", ["zoo"]])
 add_attraction("São Paulo, Brazil", ["Pátio do Colégio", ["historical site"]])
 add_attraction("Cairo, Egypt", ["Pyramids of Giza", ["monument", "historical site"]])
 add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
+
+def find_attractions(destination, interests):
+  attractions_with_interest = []
+  destination_index = get_destination_index(destination)
+  attractions_in_city = attractions[destination_index]
+
+  for results in attractions_in_city:
+    possible_attraction = []
+    attraction_tags = []
+    possible_attraction += results
+    attraction_tags += results[1]
+    for each_interest in interests:
+      if each_interest in attraction_tags:
+        attractions_with_interest.append(possible_attraction[0])
+
+  return attractions_with_interest
+
+
+
+
+
+
 
