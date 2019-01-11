ed80cadeb06421cd667b439a217225b11d76d4c0
Sam King <samuel-king@live.com.au>
2019-01-11 14:04:43 +1100

    Added function to generate message for traveler and present attractions they might be interested in.

--- The-Boredless-Tourist.py
+++ The-Boredless-Tourist.py
@@ -33,7 +33,7 @@ def add_attraction(destination, attraction):
 
 add_attraction('Los Angeles, USA', ['Venice Beach', ['beach']])
 add_attraction("Paris, France", ["the Louvre", ["art", "museum"]])
-add_attraction("Paris, France", ["Arc de Triomphe", ["historical site", "monument"]])
+add_attraction("Paris, France", ["the Arc de Triomphe", ["historical site", "monument"]])
 add_attraction("Shanghai, China", ["Yu Garden", ["garden", "historical site"]])
 add_attraction("Shanghai, China", ["Yuz Museum", ["art", "museum"]])
 add_attraction("Shanghai, China", ["Oriental Pearl Tower", ["skyscraper", "viewing deck"]])
@@ -58,4 +58,27 @@ def find_attractions(destination, interests):
         attractions_with_interest.append(possible_attraction[0])
 
   return attractions_with_interest
+
+def get_attractions_for_traveler(traveler):
+  traveler_destination = traveler[1]
+  traveler_interests = traveler[2]
+  loop_count = 0
+
+  traveler_attractions = find_attractions(traveler_destination, traveler_interests)
+
+  interests_string = "Hi "+str(traveler[0])+", we think you'll like these places around "+traveler_destination+": "
+
+  for attraction in traveler_attractions:
+    interests_string += attraction
+    loop_count += 1
+    if loop_count < len(traveler_attractions):
+      interests_string += ", "
+    else:
+      interests_string += "."
+
+  return interests_string
+
+smills_france = get_attractions_for_traveler(['Dereck Smill', 'Paris, France', ['monument']])
+
+print(smills_france)
 
